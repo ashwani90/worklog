ed3d901e34e2f94ba5d86a28eefaf9d9fb3189d9
Ashwani <ashwani90125@gmail.com>
2023-04-22 22:59:12 +0530

    Work on add functionality for admin

--- worklog/logs/admin.py
+++ worklog/logs/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category,Type,Log
+from .models import Category,Type,Log,Note
 
 admin.site.register(Category)
-admin.site.register(Type)
-admin.site.register(Log)
+admin.site.register(Note)
+
+@admin.action(description="Mark as completed")
+def make_completed(modeladmin, request, queryset):
+    queryset.update(completed=True)
+    
+# class LogInline(admin.TabularInline):
+#     model = Log
+    
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "completed"]
+    ordering = ["name"]
+    actions = [make_completed]
+    # inlines = [
+    #     LogInline
+    # ]
+    
+class LogAdmin(admin.ModelAdmin):
+    list_display = ["description", "type"]
+    
+
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Log, LogAdmin)
+admin.site.site_header = "Work Log"
+admin.site.site_title = "Work Log"
+admin.site.index_title = "Work Log"
--- worklog/logs/models.py
+++ worklog/logs/models.py
@@ -10,27 +10,33 @@ class Timestampable(models.Model):
     
 class Category(Timestampable):
     name = models.CharField(max_length=250)
-    description =  models.CharField()
+    description =  models.TextField()
     
     def __str__(self):
         return self.name
     
 class Type(Timestampable):
     name = models.CharField(max_length=250)
-    description = models.CharField()
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
     
     def __str__(self):
         return self.name
 
 class Log(Timestampable):
-    description = models.CharField()
+    description = models.TextField()
     type = models.ForeignKey(Type,on_delete=models.DO_NOTHING)
     category = models.ForeignKey(Category,on_delete=models.DO_NOTHING)
     time_spent = models.IntegerField()
     
     def __str__(self):
-        return self.description
+        return self.description + ' : ' + str(self.time_spent) + ' : ' + self.created_at.strftime("%d/%m/%Y")
     
-
+class Note(Timestampable):
+    description = models.TextField()
+    heading = models.CharField()
+    
+    def __str__(self):
+        return self.heading
     
     
